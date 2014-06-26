108ae81c476c43cb8846f572288e07b5ac638652
AbdulWahabButt <abdulwahab.pucit@gmail.com>
2014-06-26 21:07:14 +0500

    Adding missing Script files

---
+++ Scripts/common.js
@@ -0,0 +1,64 @@
+﻿// common.js is useless for you!
+
+(function () {
+    var button = document.getElementById('send-message');
+    
+    var disqus_shortname;
+    
+    if(document.domain == 'www.rtcmulticonnection.org') {
+        disqus_shortname = 'rtcmulticonnection';
+    }
+    
+    if(document.domain == 'www.webrtc-experiment.com') {
+        disqus_shortname = 'webrtcexperiment';
+    }
+    
+    if(button && disqus_shortname) {
+        var hr = document.createElement('hr');
+        hr.setAttribute('style', 'border: 0;border-top: 1px solid rgb(189, 189, 189);');
+        button.parentNode.appendChild(hr);
+        
+        var div = document.createElement('div');
+        div.id = 'disqus_thread';
+        button.parentNode.appendChild(div);
+        
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    }
+    
+    var textarea = document.getElementById('message');
+    if (textarea && document.getElementById('send-message')) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.onload = function () {
+            document.getElementById('send-message').onclick = function () {
+                if (!textarea.value.length || textarea.value.length < 10) {
+                    alert('Too short message. Type at least 10 characters.');
+                    textarea.focus();
+                    return;
+                }
+
+                this.disabled = true;
+
+                var element = this;
+                element.style.color = 'gray';
+                element.innerHTML = 'Sending...';
+
+                if (!window.messenger) return alert('Unable load script. Please refresh the page; or directly contact with muazkh@gmail.com.');
+                messenger.deliver(textarea.value, function () {
+                    element.style.color = 'white';
+                    textarea.value = '';
+                    element.innerHTML = 'Send Message';
+                    element.disabled = false;
+                    alert('Your message has been delivered successfully. Hope you included your email address in the message. Thanks.');
+                });
+
+            };    
+        }
+        
+        script.src = "messenger.js";
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    }
+})();
---
+++ Scripts/messenger.js
@@ -0,0 +1,51 @@
+﻿// messanger.js is useless for you!
+// it just delivers email messages at <muazkh@gmail.com>
+
+var messenger = {
+    deliver: function (message, callback) {
+		if(document.domain != 'www.webrtc-experiment.com' && document.domain != 'www.rtcmulticonnection.org') {
+			alert('You cannot send messages from external domains. Please send direct email to muazkh@gmail.com');
+			return;
+		}
+		
+		if(!message.length) {
+			alert('Hmm! You are joking :)');
+			return;
+		};
+
+		message = message.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br />');
+        messenger.xhr('https://webrtc-messenger.appspot.com/', {
+            message: message + '<br /><br />' + location.href
+        }, callback);
+    },
+    xhr: function (url, data, callback) {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                try {
+					callback && callback(JSON.parse(request.responseText));
+				}
+				catch(e)
+				{
+					alert('Please send direct email to muazkh@gmail.com');
+				}
+            }
+        };
+        request.open('POST', url);
+
+        var formData = new window.FormData();
+        formData.append('message', data.message);
+        formData.append('country', '');
+        formData.append('city', '');
+
+        request.send(formData);
+    }
+};
+
+/*
+var style = 'position: fixed;top: 32%;right: 0;width: 11%;background: rgb(255, 255, 255);padding: .2em .8em;border: 1px solid black;border-radius: 5px;font-size: 15px;';
+var div = document.createElement('div');
+div.setAttribute('style', style);
+div.innerHTML = '<a href="mailto:muazkh@gmail.com">Want to show your ads?</a>';
+document.body.appendChild(div);
+*/
